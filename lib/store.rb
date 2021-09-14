824c5de20b4c92b5880bc17f31e0703085b1e847
Jonathan Choi <jonchoi11@gmail.com>
2021-09-14 02:55:35 +0000

    add validations for both models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :sells_clothing
+
+  def sells_clothing
+    if !mens_apparel && !womens_apparel
+      errors.add(:sells_clothing, "The store does not currently sell men's or women's clothing.")
+    end
+  end
 end
